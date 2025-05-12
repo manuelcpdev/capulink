99f08c1fac681e129ac551d31bdab018b2221e0e
manuelcpdev <manuel@capulink.com>
2025-05-12 21:20:22 +0200

    Update: Codificación de test para LigazonsService

--- capulink/src/app/ligazons/ligazons.service.spec.ts
+++ capulink/src/app/ligazons/ligazons.service.spec.ts
@@ -1,16 +1,46 @@
 import { TestBed } from '@angular/core/testing';
 
 import { LigazonsService } from './ligazons.service';
+import { provideHttpClient } from '@angular/common/http';
+
+import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing'
+
+import { provideRouter } from '@angular/router';
+import { firstValueFrom } from 'rxjs';
+import { AutenticacionService } from '../autenticacion.service';
 
 describe('LigazonsService', () => {
   let service: LigazonsService;
+  let httpTesting: HttpTestingController;
+  let autenticacionService: AutenticacionService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting(),
+        provideRouter([]),
+        AutenticacionService
+      ]
+    });
     service = TestBed.inject(LigazonsService);
+    httpTesting = TestBed.inject(HttpTestingController);
+    autenticacionService = TestBed.inject(AutenticacionService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should make a get request', () => {
+    const obterLigazonsPublicas$ = service.obterLigazonsPublicas();
+    const obertLigazonsPublicasPromise = firstValueFrom(obterLigazonsPublicas$);
+
+    const req = httpTesting.expectOne(`${autenticacionService.api}/ligazons/publicas`, 'Request to ligazóns')
+
+    expect(req.request.method).toBe('GET');
+
+    //req.flush()
+  });
+
 });
