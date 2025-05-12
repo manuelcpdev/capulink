6aeddef9e9f149b422e204543b51eca15fb4962c
manuelcpdev <manuel@capulink.com>
2025-05-12 21:18:52 +0200

    Update: Modificado ConexionComponent para usar BaseFormAutenticacionComponent
    Utilización dun formulario base en ConexionComponent

--- capulink/src/app/conexion/conexion.component.ts
+++ capulink/src/app/conexion/conexion.component.ts
@@ -3,11 +3,12 @@ import { AutenticacionService } from '../autenticacion.service';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { NgClass, NgFor, NgIf } from '@angular/common';
 import { Router } from '@angular/router';
+import { BaseFormAutenticacionComponent } from "../base-form-autenticacion/base-form-autenticacion.component";
 
 @Component({
   selector: 'app-conexion',
   standalone: true,
-  imports: [ReactiveFormsModule, NgIf, NgFor],
+  imports: [ReactiveFormsModule, NgIf, NgFor, BaseFormAutenticacionComponent],
   templateUrl: './conexion.component.html',
   styleUrl: './conexion.component.scss'
 })
@@ -37,7 +38,7 @@ export class ConexionComponent {
     this.autenticacionService.iniciarSesion(formulario)?.subscribe({
       next: (resposta) => {
         this.autenticacionService.usuarioConectadoSubject.next(true);
-        this.autenticacionService.eAdminSubject.next(resposta.eAdmin); // o el valor correspondiente
+        this.autenticacionService.eAdminSubject.next(resposta.eAdmin);
 
         localStorage.setItem('usuarioConectado', 'true');
         localStorage.setItem('eAdmin', resposta.eAdmin.toString());
@@ -50,7 +51,7 @@ export class ConexionComponent {
           this.errosServidor[key] = resposta['error'][key]
         }
         this.autenticacionService.usuarioConectadoSubject.next(false);
-        this.autenticacionService.eAdminSubject.next(false); // o el valor correspondiente
+        this.autenticacionService.eAdminSubject.next(false);
 
         localStorage.setItem('usuarioConectado', 'false');
         localStorage.setItem('eAdmin', 'false');
