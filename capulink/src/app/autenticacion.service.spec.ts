94dd782e5e93dcfc565681f9052c7342ecfaf372
manuelcpdev <manuel@capulink.com>
2025-05-12 21:13:37 +0200

    Update: Codificación dun test para AutenticacionService

--- capulink/src/app/autenticacion.service.spec.ts
+++ capulink/src/app/autenticacion.service.spec.ts
@@ -1,16 +1,31 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AutenticacionService } from './autenticacion.service';
+import { HttpClient, provideHttpClient } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
 
 describe('AutenticacionService', () => {
   let service: AutenticacionService;
+  let httpTesting: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting(),
+      ]
+    });
     service = TestBed.inject(AutenticacionService);
+    httpTesting =TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('Get CSRF token', () => {
+    const req = service.obterXSRF();
+    expect(req).toBeDefined()
+    console.log(req)
   });
 });
