a54c6a0731bf6d3cb5545773a49f3b1751e3ae64
manuelcpdev <manuel@capulink.com>
2025-05-16 22:48:22 +0200

    Update: Engadido dominio ós arquivos referidos a CORS e Sanctum en Laravel

--- capulink-laravel/config/cors.php
+++ capulink-laravel/config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost', 'http://localhost:4200', 'http://localhost:4201', '192.168.1.120' , 'http://192.168.1.120', 'http://192.168.1.120:4201', 'http://192.168.1.120:4200'],
+    'allowed_origins' => ['https://capulink.com', 'capulink.com', 'http://localhost', 'http://localhost:4200', 'http://localhost:4201', '192.168.1.120' , 'http://192.168.1.120', 'http://192.168.1.120:4201', 'http://192.168.1.120:4200'],
 
     'allowed_origins_patterns' => [],
 
