88ae9267d5aceb9f704212590a88ab1789276adf
manuelcpdev <manuel@capulink.com>
2025-05-29 23:02:31 +0200

    Update: Engadido cartafol "administracion/pages"

---
+++ capulink/src/app/administracion/pages/administracion/administracion.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { MenuOpcions } from '../../enums/menu-opcions';
+import { CommonModule, NgFor, NgIf } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { TabsComponent } from '../../../shared/components/tabs/tabs.component';
+
+@Component({
+  selector: 'app-administracion',
+  templateUrl: './administracion.component.html',
+  styleUrls: ['./administracion.component.scss'],
+  standalone: true,
+  imports: [NgIf, CommonModule, ReactiveFormsModule, TabsComponent],
+})
+export class AdministracionComponent {
+  menuOpcions = MenuOpcions; // Facer dispoñible o enum no template
+  opcionActual: string = MenuOpcions.Inicio; // A opción predeterminada
+
+  opcionsMenu = Object.values(MenuOpcions); // Obter as opcións do enum como lista de strings
+
+  cambiarOpcion(opcion: string) {
+    this.opcionActual = opcion; // Cambiar a opción actual
+  }
+}
